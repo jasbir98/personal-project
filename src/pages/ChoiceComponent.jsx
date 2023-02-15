82c2290142b9ec9a967b59a4d3e5d6b5545bef7c
Negi <negi@192.168.1.12>
2023-02-15 10:25:58 +0530

    refactoring

--- src/pages/ChoiceComponent.jsx
+++ src/pages/ChoiceComponent.jsx
@@ -39,23 +39,23 @@ function ChoiceComponent({selectedId}) {
     // showContent ? eachQuestion['choiceImage'] : '../images/unknown.avif'
     return (
             <Container maxWidth="lg" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-                {!showLastMessage ? <Box sx={{ flexGrow: 1, maxHeight: "100vh", display: 'flex', alignItems: 'center', justifyContent: "center", width: '100%'}}>
+                {!showLastMessage ? <Box sx={{ flexGrow: 1, maxHeight: "100vh", display: 'flex', alignItems: 'center', justifyContent: "center", width: '100%', flexDirection: "column"}}>
+                <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                 <Box
       sx={{
         display: 'flex',
-        position: "absolute",
-        top: "40%",
         width: '100%',
         alignItems: 'center',
         justifyContent: 'center',
         bgcolor: 'background.default',
         color: 'text.primary',
         borderRadius: 1,
+        marginBottom: "40px",
         p: 3,
       }}
     >
       {questions[selectedId].questions_and_choices[currentQuestion]['questionText']}
-    </Box>    
+    </Box>          <div style={{display: "flex"}}>
                     {questions[selectedId].questions_and_choices[currentQuestion].options.map((eachQuestion, index) => (
                      <Fragment key={index}>
                         <Grid container spacing={2} columns={8}>
@@ -79,8 +79,10 @@ function ChoiceComponent({selectedId}) {
                         </Grid>
                      </Fragment>
                      
-                    ))}   
-                    {showContent ? <Button onClick={handleButtonClick} style={{position: "absolute", top: "120%"}} variant="contained">Next</Button> : null}
+                    ))}
+                    </div>
+                    </div>   
+                    {showContent ? <Button onClick={handleButtonClick} variant="contained" style={{marginTop: "40px"}}>Next</Button> : null}
                 </Box> : <img src={require("../images/bye.gif")} alt="bye"/>}
             </Container>
     )
